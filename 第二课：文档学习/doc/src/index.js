168eb2c6eaec0e4ba339ce89ca5cae95601df26b
emmalkl <emma_lkl@163.com>
2020-04-01 16:56:14 +0800

    4.4

--- 第二课：文档学习/doc/src/index.js
+++ 第二课：文档学习/doc/src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//{} 通过大括号嵌入js表达式
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+//jsx也是一个表达式
+function getGreeting(user){
+  if(user){
+    return <h1>Hello, {formatName(user)}!</h1>
+  }
+  return <h1>Hello, Stranger.</h1>;
+}
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+const user1 = {
+  firstName: 'Li',
+  lastName: 'Lin'
+};
+const element = (
+  <div>
+    <h1>
+      Hello, {formatName(user)}!
+    </h1>
+    {getGreeting(user1)}
+    <h2>定时器函数（元素渲染）It is {new Date().toLocaleTimeString()}.</h2>
+  </div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
